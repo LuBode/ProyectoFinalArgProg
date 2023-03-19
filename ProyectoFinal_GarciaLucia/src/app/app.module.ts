1a224580142598b47b55aacec89f9c6b1a336755
LuBode <luciaagarcia.16@gmail.com>
2023-03-19 11:57:16 -0300

    Seccion footer

--- ProyectoFinal_GarciaLucia/src/app/app.module.ts
+++ ProyectoFinal_GarciaLucia/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HabilidadesComponent } from './components/habilidades/habilidades.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
     AcerdeComponent,
     EducacionComponent,
     HabilidadesComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
